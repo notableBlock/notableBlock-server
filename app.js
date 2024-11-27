69188df3c8cb651e17a28a385d2420d0898d729d
choiOV <121269364+choiOV@users.noreply.github.com>
2024-11-28 03:57:03 +0900

    Feat: 사용자 데이터 MongoDB 저장 및 user 라우터 설정

--- app.js
+++ app.js
@@ -1,35 +1,53 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+require("dotenv").config();
 
-const indexRouter = require('./routes/index');
+const createError = require("http-errors");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/users");
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+try {
+  mongoose.connect(process.env.MONGO_ATLAS_URI);
+  console.log("MongoDB 연결 성공");
+} catch (error) {
+  console.log("MongoDB 연결 실패:", error);
+  next(error);
+}
 
-app.use(logger('dev'));
+if ( process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
+} else {
+  app.use(morgan("dev"));
+}
+
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
+app.use("/users", userRouter);
 
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- models/User.js
+++ models/User.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   googleId: { type: String, required: true },
-  nickname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  nickname: { type: String, unique: true },
+  picture: String,
   notes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Note" }],
   notifications: [{ type: mongoose.Schema.Types.ObjectId, ref: "Notification" }],
 });
