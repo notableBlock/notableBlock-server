07bd9bd3eef4dd91817cad344f07b8b96c112ea6
choiOV <121269364+choiOV@users.noreply.github.com>
2024-12-31 05:41:54 +0900

    Fix: 알림 삭제 시 사용자 데이터와 동기화

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,6 +1,7 @@
 const createError = require("http-errors");
 
 const Notification = require("../models/Notification");
+const User = require("../models/User");
 
 const sendNotification = async (req, res, next) => {
   res.setHeader("Content-Type", "text/event-stream");
@@ -25,15 +26,33 @@ const sendNotification = async (req, res, next) => {
 };
 
 const deleteNotification = async (req, res, next) => {
+  const { _id: userId } = req.user;
   const { notificationId } = req.params;
 
   try {
-    await Notification.findByIdAndDelete(notificationId);
+    const deletedNotification = await Notification.findByIdAndDelete(notificationId);
+    if (!deletedNotification) {
+      next(createError(404, "삭제할 알림을 찾을 수 없습니다."));
+      return;
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      next(createError(404, "사용자를 찾을 수 없습니다."));
+      return;
+    }
+
+    const index = user.notifications.findIndex(
+      (notificationId) => notificationId.toString() === deletedNotification._id.toString()
+    );
+    if (index !== -1) {
+      user.notifications.splice(index, 1);
+      await user.save();
+    }
 
     res.status(200).json({ message: "알림을 삭제했습니다." });
   } catch (err) {
     next(createError(500, "알림을 삭제하는데 실패했습니다."));
-    return;
   }
 };
 
