bb6806e3cc600b7ae67a8d3051e0c9e61a944c83
choiOV <121269364+choiOV@users.noreply.github.com>
2025-03-19 18:47:40 +0900

    Fix: 노트 제목이 10글자를 초과할 경우 생략(…) 처리 추가

--- utils/getNoteTitle.js
+++ utils/getNoteTitle.js
@@ -1,5 +1,9 @@
-const getNoteTitle = (blocks) =>
-  (blocks.find((block) => ["h1", "h2", "h3", "p"].includes(block.tag))?.html ?? "제목이 없는") +
-  " 노트";
+const getNoteTitle = (blocks) => {
+  const title =
+    blocks.find((block) => ["h1", "h2", "h3", "p"].includes(block.tag))?.html ?? "제목 없음";
+  const noteTitle = `${title} 노트`;
+
+  return noteTitle.length > 10 ? `${noteTitle.slice(0, 10)}... 노트` : noteTitle;
+};
 
 module.exports = getNoteTitle;
