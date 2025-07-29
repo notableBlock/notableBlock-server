a82536ef2db56e7391b8f5426cdd733f3b9e7073
choiOV <121269364+choiOV@users.noreply.github.com>
2025-07-29 17:44:20 +0900

    Fix: getCurrentDate | 타임존 설정 추가로 배포 환경에서도 KST 기준으로 저장되도록 변경

--- utils/getCurrentDate.js
+++ utils/getCurrentDate.js
@@ -1,7 +1,12 @@
 const dayjs = require("dayjs");
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const getCurrentDate = () => {
-  return dayjs().format("YYYY-MM-DD HH:mm:ss");
+  return dayjs().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss");
 };
 
 module.exports = getCurrentDate;
