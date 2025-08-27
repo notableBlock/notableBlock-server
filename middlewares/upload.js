943c306a3c8a7eaad0e299be1b08e04024f09ca0
choiOV <121269364+choiOV@users.noreply.github.com>
2025-08-27 21:45:21 +0900

    Fix: CI/CD | 테스트 환경에서 S3 업로드 비활성화

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,11 +1,8 @@
 const multer = require("multer");
-const multerS3 = require("multer-s3");
 const path = require("path");
 const fs = require("fs");
 const os = require("os");
 const createError = require("http-errors");
-
-const { s3 } = require("../services/s3Services");
 
 const objectId = require("../utils/objectId");
 
@@ -14,24 +11,14 @@ const diskStorage = multer.diskStorage({
     const tempDirectory = path.join(process.env.TEMP_DIR || os.tmpdir(), "notableBlock-temp");
     fs.mkdirSync(tempDirectory, { recursive: true });
 
-    cb(null, tempDirectory);
     req.tempDirectory = tempDirectory;
+    cb(null, tempDirectory);
   },
 
   filename: async (req, file, cb) => {
     const decodedName = Buffer.from(file.originalname, "latin1").toString("utf-8");
 
     cb(null, decodedName);
-  },
-});
-
-const s3Storage = multerS3({
-  s3,
-  bucket: process.env.S3_BUCKET,
-  key: (req, file, cb) => {
-    const decodedName = Buffer.from(file.originalname, "latin1").toString("utf-8");
-
-    cb(null, `${objectId()}-${decodedName}`);
   },
 });
 
@@ -54,7 +41,25 @@ const fileFilter = (req, file, cb) => {
   return cb(null, false);
 };
 
-const chooseStorage = (req) => (req.query.s3 === "true" ? s3Storage : diskStorage);
+const chooseStorage = (req) => {
+  const isUseS3 = process.env.USE_S3 === "true" && req.query.s3 === "true";
+  if (!isUseS3) return diskStorage;
+
+  const multerS3 = require("multer-s3");
+  const { s3 } = require("../services/s3Services");
+
+  const s3Storage = multerS3({
+    s3,
+    bucket: process.env.S3_BUCKET,
+    key: (req, file, cb) => {
+      const decodedName = Buffer.from(file.originalname, "latin1").toString("utf-8");
+
+      cb(null, `${objectId()}-${decodedName}`);
+    },
+  });
+
+  return s3Storage;
+};
 
 const dynamicUpload = (fieldName, type) => {
   return (req, res, next) => {
@@ -74,7 +79,7 @@ const dynamicUpload = (fieldName, type) => {
     uploadSelector(req, res, (err) => {
       if (err) {
         console.log(err);
-        return next(createError(400), "파일 업로드 도중 오류가 발생했어요.");
+        return next(createError(400, "파일 업로드 도중 오류가 발생했어요."));
       }
 
       next();
